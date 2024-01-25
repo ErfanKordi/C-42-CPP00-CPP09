ab64a510dc885cffdbd99c859559a839167384d2
Erfan <erfan.12.kordi@gmail.com>
2024-01-25 09:32:42 +0100

    ex05 finish

---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekordi <ekordi@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 08:59:23 by ekordi            #+#    #+#             */
+/*   Updated: 2024/01/25 09:29:46 by ekordi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("ERROR");
+	harl.complain("ERRfOR");
+	harl.complain("WARNING");
+	return 0;
+}
