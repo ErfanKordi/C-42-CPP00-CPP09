c7e2668a52737a7a77bf6078cef971ce29f8cd77
ErfanKordi <erfan.12.kordi@gmail.com>
2024-06-15 23:23:20 +0200

    cpp07

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,26 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+#include <stdexcept>
+
+template <typename T>
+class Array
+{
+private:
+	unsigned int _size;
+	T *_array;
+
+public:
+	Array();
+	Array(unsigned int n);
+	Array(const Array &other);
+	Array &operator=(const Array &other);
+	~Array();
+
+	unsigned int size() const;
+	T &operator[](unsigned int index);
+	const T &operator[] (unsigned int index) const;
+};
+
+#endif // ARRAY_HPP
